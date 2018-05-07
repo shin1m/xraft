9320a4fdc12809ac31ad0b86ec54fb261630855b
Shin-ichi MORITA <shin1morita@gmail.com>
2018-05-08 07:45:25 +0900

    Improved to use perfect forwarding.

--- xemmai/geometry.h
+++ xemmai/geometry.h
@@ -12,10 +12,10 @@ struct t_type_of<xraft::t_point> : t_type
 	typedef xemmaix::xraft::t_extension t_extension;
 
 	template<typename T_extension, typename T>
-	static t_scoped f_transfer(T_extension* a_extension, T a_value)
+	static t_scoped f_transfer(T_extension* a_extension, T&& a_value)
 	{
 		t_scoped object = t_object::f_allocate(a_extension->template f_type<typename t_fundamental<T>::t_type>());
-		object.f_pointer__(new xraft::t_point(a_value));
+		object.f_pointer__(new xraft::t_point(std::forward<T>(a_value)));
 		return object;
 	}
 	static int f_x(xraft::t_point& a_self)
@@ -48,10 +48,10 @@ struct t_type_of<xraft::t_extent> : t_type
 	typedef xemmaix::xraft::t_extension t_extension;
 
 	template<typename T_extension, typename T>
-	static t_scoped f_transfer(T_extension* a_extension, T a_value)
+	static t_scoped f_transfer(T_extension* a_extension, T&& a_value)
 	{
 		t_scoped object = t_object::f_allocate(a_extension->template f_type<typename t_fundamental<T>::t_type>());
-		object.f_pointer__(new xraft::t_extent(a_value));
+		object.f_pointer__(new xraft::t_extent(std::forward<T>(a_value)));
 		return object;
 	}
 	static unsigned f_width(xraft::t_extent& a_self)
@@ -84,10 +84,10 @@ struct t_type_of<xraft::t_rectangle> : t_type
 	typedef xemmaix::xraft::t_extension t_extension;
 
 	template<typename T_extension, typename T>
-	static t_scoped f_transfer(T_extension* a_extension, T a_value)
+	static t_scoped f_transfer(T_extension* a_extension, T&& a_value)
 	{
 		t_scoped object = t_object::f_allocate(a_extension->template f_type<typename t_fundamental<T>::t_type>());
-		object.f_pointer__(new xraft::t_rectangle(a_value));
+		object.f_pointer__(new xraft::t_rectangle(std::forward<T>(a_value)));
 		return object;
 	}
 	static int f_x(xraft::t_rectangle& a_self)
